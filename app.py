2a1c211585b3d2feda7deb439b300da464a2738b
ChanakaUOMIT <chanakauomfit@gmail.com>
2020-05-08 03:38:41 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,21 @@
+print("Hello World")
+
+# name=input('What is your name ? ')
+# print("Hi "+name)
+
+# name=input("What is your name? ")
+# favorite_color=input("hey, "+name+ " What is your favourite color? ")
+# print("Analysing...")
+# print(name+ " likes "+favorite_color)
+
+# birth_year=input("Birth Year : ")
+# print(type(birth_year))
+# age=2020 - int(birth_year)
+# print(type(age))
+# print("Age : ",age)
+
+weight_lbs=input("weight (lbs): ")
+print(type(weight_lbs))
+weight_kg =int(weight_lbs)*0.45
+print(type(weight_kg))
+print(weight_kg , " kg")
